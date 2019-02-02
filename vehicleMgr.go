50ba5f419141707bc83ab3fb443a69cf787a5815
DenysNahurnyi <dnahurnyi@gmail.com>
2019-02-02 12:39:28 +0200

    Add EndRide function to transit vehicle from Riding to Ready state

--- vehicleMgr.go
+++ vehicleMgr.go
@@ -1,14 +1,23 @@
 package vehiclemanagement
 
-import "errors"
-
 // Ride func allow user to ride a vehicle
 func Ride(user *User, vehicle *Vehicle) error {
 	// Do not check role of user, everyone allowed
-	// Check status of vehicle
+	// Check status of the vehicle
 	if vehicle.GetState() != Ready {
-		return errors.New("State transition is not possible, vehicle state is " + vehicle.GetState().String() + ", state " + Ready.String() + " is needed.")
+		return StateTransitionErr(vehicle.GetState(), Ready)
 	}
 	vehicle.SetState(Riding)
+	return nil
+}
+
+// EndRide func allow user to return vehicle form riding to ready state
+func EndRide(user *User, vehicle *Vehicle) error {
+	// Do not check role of user, everyone allowed
+	// Check status of the vehicle
+	if vehicle.GetState() != Riding {
+		return StateTransitionErr(vehicle.GetState(), Riding)
+	}
+	vehicle.SetState(Ready)
 	return nil
 }
--- vehicleMgr_test.go
+++ vehicleMgr_test.go
@@ -24,3 +24,30 @@ func TestRide(t *testing.T) {
 		t.Error("Vehicle state transition is broken, vehicle state change is wrong")
 	}
 }
+
+// TestEndRide test whether user can end it's ride
+func TestEndRide(t *testing.T) {
+	idGenerator := NewGenerator()
+
+	admin := NewUser(idGenerator.GenerateID(), Admin)
+
+	vehicle := NewVehicle(idGenerator.GenerateID(), Ready, 100)
+
+	// Riding -> Ready
+	err := Ride(admin, vehicle)
+	if err != nil {
+		t.Error("Failed to ride a vehicle")
+	}
+	err = EndRide(admin, vehicle)
+	if err != nil {
+		t.Error("Failed to end the ride of a vehicle")
+	}
+	// Ready -> ???
+	err = EndRide(admin, vehicle)
+	if err == nil {
+		t.Error("Vehicle state transition is broken, end ride for a vehicle that is already in ready state should not be possible")
+	}
+	if vehicle.GetState() != Ready {
+		t.Error("Vehicle state transition is broken, vehicle state change is wrong")
+	}
+}
