aa5610261c2adc89a28cef6a5bb7e7953a2e692b
DenysNahurnyi <dnahurnyi@gmail.com>
2019-02-02 13:20:52 +0200

    Create role comparison function

--- role.go
+++ role.go
@@ -10,6 +10,19 @@ const (
 	Admin
 )
 
+func (s Role) String() string {
+	roleStr := "Role type is invalid"
+	switch s {
+	case EndUser:
+		roleStr = "EndUser"
+	case Hunter:
+		roleStr = "Hunter"
+	case Admin:
+		roleStr = "Admin"
+	}
+	return roleStr
+}
+
 // User that has some role
 type User struct {
 	ID   string
@@ -27,4 +40,13 @@ func NewUser(id string, role Role) *User {
 		ID:   id,
 		role: role,
 	}
+}
+
+// Since we have hierarchical structure of roles, we can create levels of roles where the highest can do anything the lower can
+// AppropriateRoleLevel check wheter user can do actions appropriate to some neededLevel role
+func AppropriateRoleLevel(action string, currentRole Role, neededLevel Role) error {
+	if currentRole < neededLevel {
+		RolePermissionErr(action, currentRole, neededLevel)
+	}
+	return nil
 }
