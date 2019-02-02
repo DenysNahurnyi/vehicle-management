d57cf07ed15a6bb0b1f399a10b4fbba95dfb1244
DenysNahurnyi <dnahurnyi@gmail.com>
2019-02-02 11:45:15 +0200

    Extend vehicle functionality

--- vehicle.go
+++ vehicle.go
@@ -9,7 +9,7 @@ type State int
 const (
 	Ready State = iota
 	BatteryLow
-	Bountu
+	Bounty
 	Riding
 	Collected
 	Dropped
@@ -18,14 +18,45 @@ const (
 	Unknown
 )
 
+func (s State) String() string {
+	stateStr := "State type is invalid"
+	switch s {
+	case Ready:
+		stateStr = "Ready"
+	case BatteryLow:
+		stateStr = "BatteryLow"
+	case Bounty:
+		stateStr = "Bounty"
+	case Riding:
+		stateStr = "Riding"
+	case Collected:
+		stateStr = "Collected"
+	case Dropped:
+		stateStr = "Dropped"
+	case ServiceMode:
+		stateStr = "ServiceMode"
+	case Terminated:
+		stateStr = "Terminated"
+	case Unknown:
+		stateStr = "Unknown"
+	}
+	return stateStr
+}
+
 // Vehicle that has some state
 type Vehicle struct {
 	ID        string
 	updatedAt time.Time
 	state     State
+	battery   uint8 // Not conventional, but for the sake of saving memory
 }
 
 // GetState func return current state of the vehicle
 func (v *Vehicle) GetState() State {
 	return v.state
+}
+
+// SetState func set incomming state to the vehicle
+func (v *Vehicle) SetState(state State) {
+	v.state = state
 }
